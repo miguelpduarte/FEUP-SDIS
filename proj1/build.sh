33e1d3cfc235e7ac49e942c3c41589e95813da68
miguelpduarte <miguelpduarte98@gmail.com>
2019-04-11 12:37:25 +0100

    Improved project helper scripts

---
+++ proj1/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+COMPILE_DIR=out/production/proj1
+
+mkdir -p $COMPILE_DIR
+# Build Main class of "RMI Side"
+javac -cp src src/base/PeerMain.java -d $COMPILE_DIR
+# Build Main class of "TestApp side"
+javac -cp src src/base/TestApp.java -d $COMPILE_DIR
--- proj1/start_peers.sh
+++ proj1/start_peers.sh
@@ -1,30 +1,59 @@
 #!/usr/bin/env bash
 
+# Have to use bash for regex I think. If already using bash and [[ might as well use it for everything - thus [ is not used
+
 NUMBER_REGEX=^[0-9]+$
 
 cd $(dirname $0)
 
 FULL_PATH="$(pwd -P)"
 
+# Runtime definitions
 CLASS_PATH="/out/production/proj1"
 PEER_CLASS="base.PeerMain"
+PEER_PREFIX="Peer-" # To avoid collisions with other RMI declarations
 
-PEER_PREFIX="Peer-"
-
-if ! [[ $1 =~ $NUMBER_REGEX ]]; then
-    echo "Error: The number of peers must be a positive integer!"
-    echo "Usage: $0 n_peers [--tile [n_cols]]"
-    exit 1
+# Ensuring project compilation (assumes that both shell scripts are in the same directory)
+echo "Building project..."
+sh "$FULL_PATH/build.sh"
+if [[ $? != 0 ]]; then
+    echo "Error building the project"
+    exit 2
 fi
 
-if [[ $2 == "--tile" ]]; then
-    echo "Enabling tiling mode using i3";
-    if [[ $3 =~ $NUMBER_REGEX ]]; then
-        TILE_COLS=$3; echo "Using tiling of $3 columns"
-    else
-        TILE_COLS=3; echo "Using default tiling of 3 columns"
-    fi
-fi
+while [[ $# > 0 ]]; do
+    case $1 in
+    "--tile" | "-t")
+	    echo "Enabling tiling mode using i3";
+        if (( $# >= 2 )) && [[ $2 =~ $NUMBER_REGEX ]]; then
+            TILE_COLS=$2; echo "Using tiling of $2 columns"
+            shift
+        else
+            TILE_COLS=3; echo "Using default tiling of 3 columns"
+        fi
+        shift
+	;;
+	"-kill" | "-k")
+	    echo "Killing the previously running peers"
+	    # (assumes that both shell scripts are in the same directory)
+        sh "$FULL_PATH/kill_peers.sh"
+        # Error is ignored because no peers results in error
+        shift
+	;;
+    *)
+	if ! [[ $1 =~ $NUMBER_REGEX ]]; then
+	    echo "Error: The number of peers must be a positive integer!"
+	    echo "Usage: $0 n_peers [--tile|-t [n_cols]] [--kill|-k]"
+	    echo "(Note: Tiling options require i3wm)"
+	    exit 1
+	else
+	    echo "Starting $1 peers."
+	    N_PEERS=$1
+	    shift
+	fi
+	;;
+    esac
+done
 
 # Application arguments
 PROTOCOL_VERSION="1.0"
@@ -40,8 +69,8 @@ MDB_PORT="8824"
 MDC_HOSTNAME="228.25.25.25"
 MDC_PORT="8825"
 
-for (( i = 0; i < $1; i++ )); do
-    # i3 tiling, ignore
+for (( i = 0; i < N_PEERS; i++ )); do
+    # i3 tiling
     if [[ -n ${TILE_COLS+x} ]]; then
         col=$((i % TILE_COLS))
         # echo "my col: $col"
