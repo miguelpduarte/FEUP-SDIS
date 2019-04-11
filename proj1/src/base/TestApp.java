d056410b28297322f49d161dd513816e0f2cf603
xRuiAlves <up201606746@fe.up.pt>
2019-04-11 22:09:24 +0100

    Added arguments verification to peer app

--- proj1/src/base/TestApp.java
+++ proj1/src/base/TestApp.java
@@ -9,11 +9,10 @@ import java.util.Arrays;
 public class TestApp {
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.err.println("Not enough arguments");
+            System.err.println("usage: <rmi_peer_ap> <sub_protocol>\n" +
+                    "Supported protocols: BACKUP, RESTORE, RESTOREENH, DELETE, RECLAIM, STATE");
             System.exit(1);
         }
-
-        // TODO: Add argument validation and print usage, etc etc
 
         String rmi_peer_ap = args[0];
         String sub_protocol = args[1];
@@ -25,28 +24,60 @@ public class TestApp {
 
             switch (sub_protocol) {
                 case "BACKUP":
+                    if (args.length != 4) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> BACKUP <file_path> <replication_degree>");
+                        System.exit(1);
+                    }
                     peer.backup(operands[0], Integer.parseInt(operands[1]));
                     break;
                 case "RESTORE":
+                    if (args.length != 3) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> RESTORE <file_path>");
+                        System.exit(1);
+                    }
                     peer.restore(operands[0]);
                     break;
                 case "RESTOREENH":
+                    if (args.length != 3) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> RESTOREENH <file_path>");
+                        System.exit(1);
+                    }
                     peer.restoreEnhanced(operands[0]);
+                    break;
                 case "DELETE":
+                    if (args.length != 3) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> DELETE <file_path>");
+                        System.exit(1);
+                    }
                     peer.delete(operands[0]);
                     break;
                 case "RECLAIM":
+                    if (args.length != 3) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> RECLAIM <disk_space_kbs>");
+                        System.exit(1);
+                    }
                     peer.setMaxDiskSpace(Integer.parseInt(operands[0]));
                     break;
                 case "STATE":
+                    if (args.length != 2) {
+                        System.err.println("Wrong no. of arguments");
+                        System.err.println("Usage: <peer_id> STATE");
+                        System.exit(1);
+                    }
                     System.out.println(peer.getServiceState());
                     break;
                 default:
-                    System.err.println("Invalid method!");
+                    System.err.println("Invalid subprotocol\n" +
+                            "Supported protocols: BACKUP, RESTORE, RESTOREENH, DELETE, RECLAIM, STATE");
                     System.exit(2);
             }
         } catch (RemoteException e) {
-            System.out.println("uh oh, remote spaghetti-ohs");
+            System.out.println("Remote exception");
             e.printStackTrace();
         } catch (NotBoundException e) {
             e.printStackTrace();
