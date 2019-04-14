c058e190d54a280a8ca78759cabdc78b468e089b
miguelpduarte <miguelpduarte98@gmail.com>
2019-04-14 15:57:12 +0100

    updating readme and build script

--- proj1/build.sh
+++ proj1/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-COMPILE_DIR=out/production/proj1
+cd $(dirname $0)
 
-mkdir -p $COMPILE_DIR
-# Build Main class of "RMI Side"
-javac -cp src src/base/PeerMain.java -d $COMPILE_DIR
-# Build Main class of "TestApp side"
-javac -cp src src/base/TestApp.java -d $COMPILE_DIR
+sh build_peer.sh
+sh build_testapp.sh
